26a1737e2f18846d38fb9701a8d405d6f7063723
Anjali <146240+anjalithampi@users.noreply.github.com>
2025-02-03 10:55:26 +0000

    Added functionality for block commands - BLOCK commands include BEGIN, ROLLBACK, COMMIT - Added 2 input files for validation - Can validate by uncommenting code between "DEBUG LINES" comments - Updated README.md

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 ## How To Run
 
-1. `cat input_first.txt | ruby ./crud.rb` # First section for simple SET, GET, DELETE, COUNT
+1. `cat input_first.txt | ruby ./crud.rb`           # First section for simple SET, GET, DELETE, COUNT
+2. `cat input_complete_first.txt | ruby ./crud.rb`  # Complete code for BLOCKS and CRUD
+3. `cat input_complete_second.txt | ruby ./crud.rb` # Another example input file for complete code for BLOCKS and CRUD
--- crud.rb
+++ crud.rb
@@ -1,57 +1,105 @@
 #!/usr/bin/env ruby
+require 'json'
 
-elements_count_hash = Hash.new(-1)
-values_count = Hash.new(0)
+blocks_hash = Hash.new { |hash, key| hash[key] = Hash.new(0) }
+count_hash = Hash.new { |hash, key| hash[key] = Hash.new(0) }
+blocks_count = 0
+block_commands = [ "BEGIN", "COMMIT", "ROLLBACK" ]
+edit_commands = [ "GET", "COUNT", "DELETE" ]
 
 ARGF.readlines.each do |str|
   line = str.split(' ')
-  #puts line.inspect
 
-  command = line[0]
+  command = line[0].upcase
   hashkey = line[1]
   hashvalue = line[2]
 
-  #puts "COMMAND = #{command}; 1st INPUT = #{hashkey}; 2nd INPUT = #{hashvalue}\n"
+  if block_commands.include?(command) && (!hashkey.nil? || !hashvalue.nil?)
+    next
+  end
+  if edit_commands.include?(command) && (hashkey.nil? || !hashvalue.nil?)
+    next
+  end
+
+  ## -- DEBUG LINES --
+  ## puts "COMMAND = #{command}; 1st INPUT = #{hashkey}; 2nd INPUT = #{hashvalue}; blocks_count = #{blocks_count}\n"
+  ## -- DEBUG LINES --
+
   case command
+  when "BEGIN"
+    if blocks_hash[blocks_count].empty?
+      blocks_hash[blocks_count + 1] = Hash.new(0)
+      count_hash[blocks_count] = Hash.new(0)
+      count_hash[blocks_count + 1] = Hash.new(0)
+    else
+      blocks_hash[blocks_count + 1] = JSON.parse(blocks_hash[blocks_count].to_json, symbolize_names: false)
+      count_hash[blocks_count + 1] = JSON.parse(count_hash[blocks_count].to_json, symbolize_names: false)
+    end
+    blocks_count += 1
+  when "COMMIT"
+    if blocks_count.zero?
+      puts "ERROR! No corresponding BEGIN block!"
+      next
+    end
+    blocks_hash[blocks_count - 1] = JSON.parse(blocks_hash[blocks_count].to_json, symbolize_names: false)
+    count_hash[blocks_count - 1] = JSON.parse(count_hash[blocks_count].to_json, symbolize_names: false)
+    blocks_hash[blocks_count] = {}
+    count_hash[blocks_count] = {}
+    blocks_count -= 1
+
+  when "ROLLBACK"
+    if blocks_count.zero?
+      puts "ERROR! No corresponding BEGIN block!"
+      next
+    end
+    blocks_hash[blocks_count] = {}
+    count_hash[blocks_count] = {}
+    blocks_count -= 1
+
   when "SET"
     # If 'value' input is empty e.g : SET a
     if hashvalue.nil?
       # if key already exists skip to next command
-      # else, set key to -1 in elements hash
-      elements_count_hash.key?(hashkey) ? next : elements_count_hash[hashkey] = -1
+      next
     else # If command is correct e.g: SET a 10
-      # If old value is same as new value, skip to next command
-      next if(elements_count_hash[hashkey] == hashvalue)
-      # If key already exists, reduce count of oldvalue in values hash
-      ##print("elements_count_hash.key?(#{hashkey})? = #{elements_count_hash.key?(hashkey)}\n")
-      if elements_count_hash.key?(hashkey)
-        oldvalue = elements_count_hash[hashkey]
-        values_count[oldvalue] -= 1
-        ##print("Old value = #{oldvalue}; values_count[#{oldvalue}]= #{values_count[oldvalue]}\n")
+      next if(blocks_hash[blocks_count][hashkey] == hashvalue) # If old value is same as new value, skip to next command
+
+      if count_hash[blocks_count].key?(hashvalue) # If hashalue already exists in count_hash, reduce its count
+        count_hash[blocks_count][hashvalue] += 1
+      elsif blocks_hash[blocks_count].key?(hashkey) # If hashkey already exists in blocks_hash, reduce its count ; adjust count_hash keys' values
+        oldvalue = blocks_hash[blocks_count][hashkey]
+        count_hash[blocks_count][oldvalue] -= 1
+        count_hash[blocks_count][hashvalue] = 1
+      else # If neither hashkey exists in blocks_hash nor hashvalue exists in count_hash
+        count_hash[blocks_count][hashvalue] = 1
       end
-      # Now set key to new value
-      elements_count_hash[hashkey] = hashvalue
-      values_count[hashvalue] += 1
-      ##print("elements_count_hash[#{hashkey}] = #{elements_count_hash[hashkey]}\n")
-      ##print("New value = #{hashvalue}; values_count[#{hashvalue}]= #{values_count[hashvalue]}\n")
+
+      blocks_hash[blocks_count][hashkey] = hashvalue # Now set key to new value
     end
+
   when "GET"
     # If key exists, then print element value, else print "NULL"
-    elements_count_hash.key?(hashkey) ? puts(elements_count_hash[hashkey]) : puts("NULL")
+    blocks_hash[blocks_count].key?(hashkey) ? puts(blocks_hash[blocks_count][hashkey]) : puts("NULL")
+
   when "COUNT"
     # If key exists, then print value count, else print "NULL"
-    values_count.key?(hashkey) ? puts(values_count[hashkey]) : puts("NULL")
-  when "DELETE"
-    # If key exists in elements hash, 
-    if elements_count_hash.key?(hashkey)
-      value_key = elements_count_hash[hashkey]
-      values_count[value_key] > 1 ? values_count[value_key] -= 1 : values_count.delete(value_key)
-      ##print("\n VALUE KEY : #{value_key} and in values_count = #{values_count[value_key]}\n\n")
-      elements_count_hash.delete(hashkey)
-    end 
-  end
-  ##print("ELEMENTS = #{elements_count_hash}\n")
-  ##print("VALUES = #{values_count}\n")
-  ##print("-------------------------------------------\n")
-end
+    count_hash[blocks_count].key?(hashkey) ? puts(count_hash[blocks_count][hashkey]) : puts("NULL")
 
+  when "DELETE"
+    # If key exists in elements hash
+    if blocks_hash[blocks_count].key?(hashkey)
+      value_key = blocks_hash[blocks_count][hashkey]
+      count_hash[blocks_count][value_key] > 1 ? count_hash[blocks_count][value_key] -= 1 : count_hash[blocks_count].delete(value_key)
+      blocks_hash[blocks_count].delete(hashkey)
+      #puts "Deleted #{hashkey}"
+    #else
+      #puts("#{hashkey} not present! Not deleted!")
+    end
+  end
+
+  ## -- DEBUG LINES --
+  ## print("\nFULL BLOCK LIST = #{blocks_hash}\n")
+  ## print("VALUES = #{count_hash}\n")
+  ## print("----------------------------------------------------------------\n")
+  ## -- DEBUG LINES --
+end
